983e2038de2c6f421d0c7c793a3ead97710bc07f
Bizz1606 <93895405+Bizz1606@users.noreply.github.com>
2021-11-09 13:13:19 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Xin Chaò Anh Sang nha! 🖐🖐🖐',
+    introDesc: `Em có điều muốn nói với Anh Sang`,
+    btnIntro: 'OK',
+    title: 'Em nghe nói anh thích em phải không ? 😙',
+    desc: 'Anh Sang không trả lời , tức là Em thích Anh đó nha :> ',
+    btnYes: 'Có, Anh thật sự thích em lắm đó 💖💖💖',
+    btnNo: '💔 Em đang mơ à ! 💔 ',
+    question:'Nói cho Em biết đi vì sao Anh thích Em 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gửi đi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Em biết mà, Em cũng thích Anh lắm 🥰. Yêu Anh Sang vcl 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Em  nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/NTN11062/'
+}
